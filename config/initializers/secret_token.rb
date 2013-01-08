a10b751bcae882dc80b269f5ee559b8234ca1799
keikun17 <keikun17@gmail.com>
2013-01-08 20:03:44 +0800

    Initialize Project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeanPassing::Application.config.secret_token = '1dbb0ded07c41a251529dd8587b082545e0558a7451770395ec8e0f42bec488c847296eb624e5842a7d96b2aa962908441f3e068ce5fe4800631d04167a1b4a9'
