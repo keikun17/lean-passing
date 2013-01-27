e8a277ca0d1bfd83a088af54c46f55de49b08feb
keikun17 <keikun17@gmail.com>
2013-01-27 22:02:33 +0800

    Rangefinder library concept.

---
+++ app/assets/javascripts/whatiwant.js
@@ -0,0 +1,36 @@
+// selector : where the message will appear.
+selector.ranges({
+
+  getMessage: function(value) {
+    // Find what `range` the value falls under then replace 
+    // selector's text with the 'message'
+
+    $(self).grep(range something something)[0].message
+
+    selector.text(getFromRange, value)
+  },
+
+  // 
+  ranges: [{
+    from: 0, to: 30,
+    message: "range is between 0 to 30"
+  }
+  ,
+  {
+    from: 31, to: 80,
+    message: "range is between 31 to 80"
+  }
+  ,
+  {
+    from: 81, to: 100,
+    message: functionName
+  }]
+
+  functionName: function(){
+    // Manipulate some thigs
+    return "range is quite high";
+  }
+
+})
+
+
