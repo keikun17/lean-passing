4a7680caba678484cf4eacad0b13a3bb177e6ce2
keikun17 <keikun17@gmail.com>
2013-01-27 20:14:28 +0800

    Lint.

--- app/assets/javascripts/poop.js
+++ app/assets/javascripts/poop.js
@@ -2,16 +2,17 @@ var prediction = '';
 $(function () {
 
   function makePrediction() {
-    if(($('#finals_total').val() !== '')){
-      prediction = 'E may score ka na ng Finals e, icompute mo nalang.' 
+      if (($('#finals_total').val() !== '')) {
+            prediction = 'E may score ka na ng Finals e, icompute mo nalang.';
+          }
+  
+      if (prediction.length > 0) {
+            $('#prediction').text(prediction);
+            $('#predict').hide('slow');
+            $('#prediction').show('fast');
+          }
     }
 
-    if(prediction.length > 0){
-      $('#prediction').text(prediction);
-      $('#predict').hide('slow');
-      $('#prediction').show('fast');
-    };
-  }
-
   $('#predict').on('click', makePrediction);
-})
+});
+
