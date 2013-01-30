52fbd83cc48e80ca9766c1e7ea72e588d5b75524
keikun17 <keikun17@gmail.com>
2013-01-31 00:23:20 +0800

    Quiz, HW, Project, Midterm.

--- app/assets/javascripts/poop.js
+++ app/assets/javascripts/poop.js
@@ -1,17 +1,44 @@
 $(function () {
 
-
-
-  function fetchFinals(prediction) {
+  function fetchFinal() {
     if (($('#finals_total').val() !== '')) {
-      return 'E may score ka na ng Finals e, icompute mo nalang.';
+      return 'E may score ka na ng Finals e, icompute mo nalang.'
+    }
+  }
+
+
+  function fetchQuiz() {
+    if (($("#quizzes_total").val() !== '') && ($("#quizzes_score").val() !== '')) {
+      return "Uy may quiz ka na"
+    }
+  }
+
+  function fetchHomework() {
+    if (($("#homework_total").val() !== '') && ($("#homework_score").val() !== '')) {
+      return "Uy may homework ka na"
+    }
+  }
+
+  function fetchMidterm() {
+    if (($("#homework_total").val() !== '') && ($("#homework_score").val() !== '')) {
+      return "Uy may midterms ka na"
+    }
+  }
+
+  function fetchProject() {
+    if (($("#project_total").val() !== '') && ($("#project_score").val() !== '')) {
+      return "Uy may midterms ka na"
     }
   }
 
   function makePrediction() {
     var prediction = [] ;
     prediction.push('Default prediction');
-    prediction.push(fetchFinals(prediction));
+    prediction.push(fetchQuiz());
+    prediction.push(fetchHomework());
+    prediction.push(fetchProject());
+    prediction.push(fetchMidterm());
+    prediction.push(fetchFinal());
 
     if (prediction !== undefined){
       if (prediction.length > 0) {
