4391d99492d2568c9c0a07f57d8b71ea8ddd8d6e
keikun17 <keikun17@gmail.com>
2013-02-01 18:19:30 +0800

    prediction now global.

--- app/assets/javascripts/poop.js
+++ app/assets/javascripts/poop.js
@@ -1,32 +1,34 @@
 $(function () {
 
+  prediction = [];
+
   function percent(a,b) {
     return(parseInt(a)/parseInt(b)*100)
   }
 
   function fetchFinal() {
     ave = percent( $("#finals_score").val(), $('#finals_total').val() );
-    if ( !isNaN(ave) ) { return(ave) }
+    if ( !isNaN(ave) ) { prediction.push(ave) }
   }
 
   function fetchQuiz() {
     ave = percent( $("#quizzes_score").val(), $('#quizzes_total').val() );
-    if ( !isNaN(ave) ) { return(ave) }
+    if ( !isNaN(ave) ) { prediction.push(ave) }
   }
 
   function fetchHomework() {
     ave =  percent ($("#homeworks_score").val() , $("#homeworks_total").val() )
-    if ( !isNaN(ave) ) { return(ave) }
+    if ( !isNaN(ave) ) { prediction.push(ave) }
   }
 
   function fetchMidterm() {
     ave =  percent ($("#midterms_score").val() , $("#midterms_total").val() )
-    if ( !isNaN(ave) ) { return(ave) }
+    if ( !isNaN(ave) ) { prediction.push(ave) }
   }
 
   function fetchProject() {
     ave =  percent ($("#projects_score").val() , $("#projects_total").val() )
-    if ( !isNaN(ave) ) { return(ave) }
+    if ( !isNaN(ave) ) { prediction.push(ave) }
   }
 
   function fetchTerrorLevel() {
@@ -34,21 +36,18 @@ $(function () {
   }
 
   function makePrediction() {
-    var prediction = [] ;
     prediction.push('Default prediction');
-    prediction.push(fetchQuiz());
-    prediction.push(fetchHomework());
-    prediction.push(fetchProject());
-    prediction.push(fetchMidterm());
-    prediction.push(fetchFinal());
-    prediction.push(fetchTerrorLevel());
+    fetchQuiz();
+    fetchHomework();
+    fetchProject();
+    fetchMidterm();
+    fetchFinal();
+    fetchTerrorLevel();
 
-    if (prediction !== undefined){
-      if (prediction.length > 0) {
-        $('#prediction').html(prediction.join('<br>'));
-        $('#predict').hide('slow');
-        $('#prediction').show('fast');
-      }
+    if (prediction.length > 0) {
+      $('#prediction').html(prediction.join('<br>'));
+      $('#predict').hide('slow');
+      $('#prediction').show('fast');
     }
   }
     
